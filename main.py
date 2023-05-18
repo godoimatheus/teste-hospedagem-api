832cc6ff431f3a7512c4627eaf92c3900f576dbd
Matheus Godoi <matheus.godoi@live.com>
2023-05-18 16:51:33 -0300

    add fig2

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def homepage():
     return flask.send_file('fig1.png')
 
 
-@app.route('/1')
+@app.route('/2')
 def fig1():
-    return flask.send_file('fig1.png')
+    return flask.send_file('fig2.png')
 
 
 if __name__ == '__main__':
